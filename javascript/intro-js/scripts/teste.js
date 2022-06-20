e643373b10742630759d28ccc8f57d24d023faaf
KMIRNS <102094427+KMIRNS@users.noreply.github.com>
2022-06-19 21:23:59 -0300

    Spread
    Introdução ao JavaScript

---
+++ javascript/intro-js/scripts/teste.js
@@ -0,0 +1,14 @@
+// COMENTÁRIO DE LINHA
+
+/* 
+COMENTÁRIO
+DE VÁRIAS LINHAS
+*/
+
+var preco = 2
+var desconto = 0.2
+preco = preco - desconto
+
+const PRECO = 2
+var total = PRECO - desconto
+// O valor não pode ser manipulado.
